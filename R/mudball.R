091c4cef4a16300f655b003801790ea4776ba988
Takeshi KISHIYAMA <kishiyama.t@gmail.com>
2018-11-26 12:37:33 +0900

    refs #16 再帰しました。 #8 極力減らし、log=FALSEで消せるようにしました。#9 #17 #18 #19 #14 は不要になりました。

--- R/mudball.R
+++ R/mudball.R
@@ -29,166 +29,77 @@
 #   Build and Reload Package:  'Ctrl + Shift + B'
 #   Check Package:             'Ctrl + Shift + E'
 #   Test Package:              'Ctrl + Shift + T'
-
-# res = anova(models[[7]],models[[6]])
-# pp = res$Pr[!is.na(res$Pr)]
-# if(length(pp)!=1){
-#   warning("何かおかしい")
-# }
-# if(pp<0.01){
-#   print("有意差")
-# }
-
-step = function(model,i=0,beeping=F,ps=list(),p=0.05){
-  # パッケージの読み込み
-  require(lme4)
-  if(beeping){
-    require(beepr)
-    beep()
-  }
-  # 終了条件のフラグ
-  is_significant = FALSE
-  is_done = FALSE
-
-  message("############is_significant_start###################")
-  # 最初ならばpsにモデルを追加する
-  # 違うならば、psが2つ以上なのでanovaにかけてis_significantを更新する
-  if(length(ps)==0){
-    old_model_name = deparse(substitute(model))
-    appender = sprintf('ps = append(ps, %s)', old_model_name)
-    eval(parse(text=appender))
-  }else{
-    res = anova(rev(ps)[[1]],rev(ps)[[2]])
-    pp = res$Pr[!is.na(res$Pr)]
-    if(pp<p){
-      is_significant = TRUE
-      print("以下の２つのモデルに有意差がありました。後者を返します")
-      # 新しい方
-      print(rev(ps)[[1]])
-      # 古い方
-      print(rev(ps)[[2]])
+require(lme4)
+require(magrittr)
+require(tidyverse)
+step.bw <- function(model,log=TRUE){
+    # rand(om effect)における最も小さいvarianceを除外したい
+    varcorr.df.rand <- 
+        model %>% VarCorr %>% as.data.frame %>% 
+        ## interceptでもrasidualでもないvarcorr
+        filter(is.na(var2))    %>% select(-var2)%>%
+        filter(!(is.na(var1))) %>% filter(var1!="(Intercept)") 
+    ## でもその前にrandomが0なら? それは再帰の基底部
+    ## 最も単純なモデルは削れないのでそのモデルを返して再帰を終了
+    model.summary <- model %>% summary
+    model.call    <- model.summary %>% "$"(call)
+    var.min.i   <-which.min(varcorr.df.rand$vcov)
+    if(log){
+        print("今のモデルは以下です。")
+        print(model.call)
+        print("summaryは以下です。")
+        print(model.summary)
+        print("以下の項を削り式を作ります。")
+        var.min     <- varcorr.df.rand[var.min.i,]
+        print(var.min)
     }
-  }
-  message("################is_significant_done###############")
-  message("現在のプロセス番号：")
-  message(i)
-  # 最小のモデルまで行きました。
-  model_summary = summary(model)
-  rand_factors = as.list(rownames(as.data.frame(model_summary$ngrps)))
-  variances = model_summary$varcor
-
-  # "stddev_Hoge" に variances$Hoge の stddev を保存する * lenght(rand_factors)
-  stds = list()
-  for(rand_factor in rand_factors){
-    # std_Hoge
-    rand_factor_std = sprintf('std_%s', rand_factor)
-    # attr(variances$Hoge,"stddev")
-    get_rand_factor_std = sprintf('attr(variances$%s,"stddev")', rand_factor)
-    # stddev_Hoge <= attr(variances$Hoge,"stddev")
-    assign(rand_factor_std,as.data.frame(eval(parse(text=get_rand_factor_std))))
-    stds = append(stds, rand_factor_std)
-  }
-  # > stds
-  # [[1]]
-  # [1] "std_ParticipantName"
-  # [[2]]
-  # [1] "std_ItemNo"
-
-  # stddev_Hoge の中から最も小さい std と、
-  # 該当する Hoge, と切片名を取得し min_list に格納する
-  # 適当に大きい数字をとりあえず置く
-  min_std = 10000
-  min_list = list()
-  for(rand_factor in rand_factors){
-    # 行数が1なら、Interceptしかない、ということ。走査範囲外。
-    if (nrow(get(sprintf('std_%s', rand_factor)))==1) next
-    # Intercept以外([-1,])で最小値を求める
-    tmp_min = min(get(sprintf('std_%s', rand_factor)) [-1,])
-    if(tmp_min<=min_std){
-      min_std=tmp_min
-      list_of_vars = get(sprintf('std_%s', rand_factor))[,1]
-      # 最も小さい値のインデックスを取得し（必ずIntercept以外）、
-      min_index = which(list_of_vars == min_std)[1]
-      # そのインデックスを持つ変数の名前を取得し、
-      min_name = rownames(get(sprintf('std_%s', rand_factor))) [min_index]
-      # リストにランダム要素、変数名、stdの値を入れる
-      min_list = c(rand_factor,min_name,min_std)
+    if(nrow(varcorr.df.rand)==0){
+        print("これ以上式を削れません")
+        return(model)
     }
-  }
-  # print(min_list)
-  # [1] "ItemNo"            "npi:aff"           "0.105571870337726"
+    ## 気を取り直して最も小さいvarianceのidを探して除外
+    var.not.min <-varcorr.df.rand[1:nrow(varcorr.df.rand)!=var.min.i,]
+    # 上で最も小さいvarianceが分かったので式から削る
+    ## まずはmodel.callからfunction, formula, dataを抽出
+    fun           <- model.call[1] %>% as.character
+    data          <- model.call[3] %>% as.character
+    formula.parts <- model.call[2] %>% as.character %>%
+                         str_replace_all(" ","") %>%
+                         strsplit(.,split="\\+\\(") %>% unlist
+    fixed.part    <- formula.parts[1]
+    ## random.partsはlistになります
+    random.parts  <- formula.parts[1:length(formula.parts)!=1]
+    ## random factor は|ではじまり)で終わる.その部分を抽出
+    random.factors <- random.parts %>%
+        ## まずrandom.partごとに|と)の場所の同定
+        str_locate(regex("[\\|]+[\\w]*[\\)]")) %>%
+        ## random.partのそれぞれの横にstartとendをbind
+        cbind(random.parts) %>% as.data.frame %>%
+        mutate(start    =as.numeric(as.character(start))+1,
+               end      =as.numeric(as.character(end))-1,
+               r.factors=str_sub(random.parts,start=start,end=end))
+    ## random factors ずつ式を立てる。rfの数に行が圧縮される
+    random.df.newlines <- random.factors %>%
+        ## group_by(random.parts) %>%
+        group_by(r.factors) %>%
+        mutate(r.effects=(r.factors %>%
+                            (function(x)
+                                var.not.min %>% filter(grp==x) %>%
+                                # var1は切片なので|の左側で+結合
+                                select(var1) %>% unlist %>% 
+                                paste(collapse="+"))),
+               r.newlines=ifelse(nchar(r.effects)!=0,
+                          sprintf("(1+%s|%s)",r.effects,r.factors),
+                          sprintf("(1|%s)",r.factors)))
 
-  is_done = is.null(min_list[2][[1]])
-
-  if(is_done || is_significant){
-    if(beeping){beep(5)}
-    # もし有意差がでたなら、古いモデルを
-    # 出ていないなら、最もシンプルなモデル（一番新しく追加されたもの）を
-    if(is_significant){
-      # 古い方
-      return(rev(ps)[[2]])
-    }else{
-      # 新しい方
-      return(rev(ps)[[1]])
+    random.parts.new <- paste(random.df.newlines$r.newlines, collapse="+")
+    formula.new <- paste(fixed.part, random.parts.new, sep="+")
+    ## 削ったので式を統合
+    model.next.str <-sprintf("%s(%s,data=%s)",fun,formula.new,data)
+    if(log){
+        print("すると次のモデルは以下となります。")
+        print(model.next.str)
     }
-  }
-
-  rownames(get(sprintf('std_%s', rand_factor)))
-  rand_intercepts = list()
-  for(rand_factor in rand_factors){
-    # min があるファクター名ならば
-    if(rand_factor==min_list[1]){
-      x = rownames(get(sprintf('std_%s', rand_factor)))[-1]
-      y = x[x!=min_list[2]]
-      if(length(y)==0){
-        rand_intercept = sprintf('(1 |%s)',rand_factor)
-      }else{
-        z = paste(y, collapse=" + ")
-        rand_intercept = sprintf('(1 + %s|%s)',z ,rand_factor)
-      }
-      rand_intercepts = append(rand_intercepts, rand_intercept)
-    }else{
-      x = rownames(get(sprintf('std_%s', rand_factor)))[-1]
-      y = x
-      if(length(y)==0){
-        rand_intercept = sprintf('(1 |%s)',rand_factor)
-      }else{
-        z = paste(y, collapse=" + ")
-        rand_intercept = sprintf('(1 + %s|%s)',z ,rand_factor)
-      }
-      rand_intercepts = append(rand_intercepts, rand_intercept)
-    }
-  }
-
-  # Rondom Factor and Intercept
-  rs = paste(rand_intercepts, collapse=" + ")
-
-  formula = as.character(model_summary$call)
-  lme_function = formula[1]
-  lme_formula = formula[2]
-  lme_data = formula[3]
-  dependent_independent = strsplit(lme_formula , "\\(")[[1]][1]
-  new_line = paste(dependent_independent,rs)
-  message("########### 新しい式づくり start ##################")
-  message(paste("The old formula is: ",lme_formula))
-  message("下がstdです。")
-  print(variances)
-  message(sprintf('最もstdが小さいのは %s の中の %s なので、これを削ります', min_list[1], min_list[2]))
-  message("その結果、以下の式が得られます")
-  message(paste("The new formula is: ",new_line))
-  message("############ 新しい式づくり end ################")
-
-  # モデルの名前を更新します
-  i = i + 1
-
-  message("############ 新しいモデル名にモデルを格納し、psも更新します ################")
-  old_model_name = deparse(substitute(model))
-  new_model_name = paste(old_model_name, as.character(i),sep = "_")
-  str_formula = sprintf('%s = lmer(%s, data=%s)', new_model_name, new_line, lme_data)
-  eval(parse(text=str_formula))
-  appender = sprintf('ps = append(ps, %s)', new_model_name)
-  eval(parse(text=appender))
-  message("############ 新しいモデル名にモデルを格納し、psも更新しました。再帰処理に入ります。 ################")
-  recall = sprintf('step(%s, i, beeping, ps=ps, p=p)', new_model_name)
-  eval(parse(text=recall))
+    model.next <- eval(parse(text=model.next.str))
+    return(c(model, step.bw(model.next,log)))
 }
